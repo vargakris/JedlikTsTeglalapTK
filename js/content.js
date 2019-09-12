13a109d82561c0e97a23f1dcb55d1eb9f52496d7
Varga Kristóf Márk <varga.kristof.mark@jedlik.helyi>
2019-09-12 13:17:43 +0200

    first commit

---
+++ js/content.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs = require("fs");
+class Content {
+    content(req, res) {
+        // favicon.ico kérés kiszolgálása:
+        if (req.url === "/favicon.ico") {
+            res.writeHead(200, { "Content-Type": "image/x-icon" });
+            fs.createReadStream("favicon.ico").pipe(res);
+            return;
+        }
+        res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+        res.write("<h1 style='color: red;'>Hello Node.js</h1>");
+        res.write("<h1 style='color: green;'>Hello TypeScript!</h1>");
+        res.write("<h1 style='color: blue;'>Hello Heroku!</h1>");
+        res.write("<h1 style='color: black;'>Hello Jedlik!</h1>");
+        res.write("<b>Fejlesztői környezet telepítésének leírása, forráskód GitHub repository:</b><br>");
+        res.write("<a href='https://github.com/nitslaszlo/JedlikTsTemplate' target='_blank'>" +
+            "https://github.com/nitslaszlo/JedlikTsTemplate</a><br>");
+        res.end();
+    }
+}
+exports.default = Content;
+//# sourceMappingURL=content.js.map
---
+++ ts/content.ts
@@ -0,0 +1,26 @@
+﻿import * as fs from "fs";
+import * as http from "http";
+import * as path from "path";
+
+export default class Content {
+
+    public content(req: http.IncomingMessage, res: http.ServerResponse): void {
+        // favicon.ico kérés kiszolgálása:
+        if (req.url === "/favicon.ico") {
+            res.writeHead(200, { "Content-Type": "image/x-icon" });
+            fs.createReadStream("favicon.ico").pipe(res);
+            return;
+        }
+
+        res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+        res.write("<h1 style='color: red;'>Hello Node.js</h1>");
+        res.write("<h1 style='color: green;'>Hello TypeScript!</h1>");
+        res.write("<h1 style='color: blue;'>Hello Heroku!</h1>");
+        res.write("<h1 style='color: black;'>Hello Jedlik!</h1>");
+
+        res.write("<b>Fejlesztői környezet telepítésének leírása, forráskód GitHub repository:</b><br>");
+        res.write("<a href='https://github.com/nitslaszlo/JedlikTsTemplate' target='_blank'>" +
+            "https://github.com/nitslaszlo/JedlikTsTemplate</a><br>");
+        res.end();
+    }
+}
